cf1fa116c2595141746fe249f8d17cd0da43cf33
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-07-21 16:21:04 -0300

    :sparkles: feat: implementar cadastro de administrador com hash de senha

--- portal-eletricista/src/admin/admin.controller.ts
+++ portal-eletricista/src/admin/admin.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Body } from '@nestjs/common';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(private service: AdminService) {}
+    
+    @Post('register')
+    register(@Body() data: { nome: string; email: string; senha: string }) {
+        return this.service.register(data);
+    }
+}
--- portal-eletricista/src/admin/admin.service.ts
+++ portal-eletricista/src/admin/admin.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    constructor(private prisma: PrismaService){}
+
+    async register(data: { nome: string; email: string; senha: string }) {
+        const hashed = await bcrypt.hash(data.senha, 10);
+        return this.prisma.admin.create({
+            data: { ...data, senha: hashed },
+        });
+    }
+}
