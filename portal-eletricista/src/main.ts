589e315ae8a107a8474533d90ecdf16090bdbe96
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-07-25 02:57:17 -0300

    :sparkles: feat: atualizar dependências do NestJS e adicionar suporte a uploads com multer

--- portal-eletricista/src/main.ts
+++ portal-eletricista/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Permite acessar http://localhost:3000/uploads/nomedoarquivo.jpg
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+app.enableCors({ origin: '*' });
+  await app.listen(3000);
 }
 bootstrap();
