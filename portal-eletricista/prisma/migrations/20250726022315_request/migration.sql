4118ee9e8de08dc6d68379c479c979b75ac07039
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-07-26 01:38:23 -0300

    :sparkles: feat: adicionar colunas fotoUrl e telefone nos modelos Cliente e Profissional

---
+++ portal-eletricista/prisma/migrations/20250726022315_request/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `cliente` ADD COLUMN `fotoUrl` VARCHAR(191) NULL;
---
+++ portal-eletricista/prisma/migrations/20250726031033_cliente/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `cliente` ADD COLUMN `telefone` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `profissional` ADD COLUMN `telefone` VARCHAR(191) NULL;
