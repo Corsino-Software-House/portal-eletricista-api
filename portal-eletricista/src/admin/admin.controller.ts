4be217be656c751fda1c5670e53ad97c10ccc099
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-09-02 18:26:33 -0300

    :zap: att. adicionando a funcionalidade de recuperação de senha por e-mail no controlador e serviço Admin

--- portal-eletricista/src/admin/admin.controller.ts
+++ portal-eletricista/src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Post,Body } from '@nestjs/common';
+import { Controller,Post,Body,Put} from '@nestjs/common';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
@@ -9,4 +9,8 @@ export class AdminController {
     register(@Body() data: { nome: string; email: string; senha: string }) {
         return this.service.register(data);
     }
+    @Put('recovery-password')
+async changePasswordByEmail(@Body() body: { email: string; novaSenha: string }) {
+    return this.service.changePasswordByEmail(body);
+}
 }
--- portal-eletricista/src/admin/admin.service.ts
+++ portal-eletricista/src/admin/admin.service.ts
@@ -12,6 +12,22 @@ export class AdminService {
             data: { ...data, senha: hashed },
         });
     }
+
+    async changePasswordByEmail(data: { email: string;  novaSenha: string }) {
+      const admin = await this.prisma.admin.findUnique({ where: { email: data.email} });
+    
+      if (!admin) {
+        throw new Error('admin não encontrado');
+      }
+    
+      const novaSenhaHash = await bcrypt.hash(data.novaSenha, 10);
+    
+      return this.prisma.admin.update({
+        where: { email: data.email },
+        data: { senha: novaSenhaHash },
+      });
+    }
+
     async findByEmail(email: string) {
     return this.prisma.admin.findUnique({ where: { email } });
   }
